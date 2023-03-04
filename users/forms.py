3d9d817234c19f3bbd2659cc14ed29f1c4cb1fed
azizlahmedi <aziz.lahmedi@neoxam.com>
2023-03-04 19:08:11 +0100

    gestion alerte sign up

--- users/forms.py
+++ users/forms.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField()
-    
+        
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+  
+    
