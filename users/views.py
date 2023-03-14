9d3753ec4224bc4d5fbaa44bfe1e1f45f18ba866
azizlahmedi <mohamedaziz.lahmedi@etudiant-fst.utm.tn>
2023-03-14 17:38:30 +0100

    ..

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import SignUpForm
+from django.contrib.auth.models import User
 
 
 def login_user(request):
@@ -27,17 +28,25 @@ def logout_user(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = SignUpForm(request.POST)
+        username=request.POST['username']
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created {username} ! You are now able to login')
             return redirect('login')
-        else:
-            messages.success(request,("Sorry, Passwords do NOT match Try Again..."))
+        elif User.objects.filter(username=username).exists():
+            messages.success(request, f'A user with that username already exists.')
             return redirect('register')
+        elif len(str(form.cleaned_data.get('password1'))) < 8:
+            messages.success(request, f'Password must be at least 8 characters long.')
+            return redirect('register')
+        else :
+            messages.success(request, f'Passwords do not match.')
+            return redirect('register')
+          
     else:
-        form = UserRegisterForm()
+        form = SignUpForm()
     return render(request, 'users/register.html',{'form':form})
 
 @login_required
